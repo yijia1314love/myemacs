6203c9ed6f91367ae10a96b54915ff2f8578155b
jiaoyifan <jiaoyifan@5i5j.com>
2023-05-24 18:19:15 +0800

    快捷键修改

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -11,7 +11,7 @@
 (define-key yafolding-mode-map (kbd "<C-return>") nil)
 (define-key yafolding-mode-map (kbd "C-c <C-M-return>") 'yafolding-toggle-all)
 (define-key yafolding-mode-map (kbd "C-c <C-S-return>") 'yafolding-hide-parent-element)
-(define-key yafolding-mode-map (kbd "C-c <C-return>") 'yafolding-toggle-element)
+(define-key yafolding-mode-map (kbd "C-c <C-RET>") 'yafolding-toggle-element)
 (provide 'init-keybindings)
 
 ;;快捷键操作
